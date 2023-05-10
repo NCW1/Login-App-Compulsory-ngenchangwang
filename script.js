010848a023827c3bfb846f599fbee33f72c61bb1
NCW1 <52585129+NCW1@users.noreply.github.com>
2023-05-10 03:07:37 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function login() {
+	const usernameInput = document.getElementById("username");
+	const passwordInput = document.getElementById("password");
+	const result = document.getElementById("result");
+
+	const isCorrectUsername = usernameInput.value === 'haris@sigmaschool.co';
+	const isCorrectPassword = passwordInput.value === 'password';
+
+	if (isCorrectUsername && isCorrectPassword) {
+		result.innerHTML = 'Welcome Haris!';
+	}
+	else if (isCorrectUsername) {
+		result.innerHTML = 'Wrong Password!';
+	}
+	else {
+		result.innerHTML = 'Wrong Username/Password!';
+	}
+}
